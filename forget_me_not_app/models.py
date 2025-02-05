a48530a6ae4feb4a9e6156022a204dc1aa6dc705
kohei-financier <ko.08financier.2mitchan4@gmail.com>
2025-02-05 16:46:59 +0900

    memoモデルの追加

--- forget_me_not_app/models.py
+++ forget_me_not_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Memo(models.Model):
+    """忘れ物メモを保存する"""
+
+    title = models.CharField(max_length=20)
+    text = models.TextField()
+    reminder_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+# ここにユーザーモデルは記載しない！（再利用性や複雑な認証をつけるなら別のAPPとして立ち上げたほうが良い）
